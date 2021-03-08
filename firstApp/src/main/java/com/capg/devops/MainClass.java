630ee86c0dc9bba756b7535879a3f959c2b848ab
Nitin Goyal <Nitin Goyal@DESKTOP-5E2HD8Q>
2021-03-08 16:13:35 +0530

    first commit

---
+++ firstApp/src/main/java/com/capg/devops/MainClass.java
@@ -0,0 +1,20 @@
+package com.capg.devops;
+
+public class MainClass {
+
+	 
+
+    public static void main(String[] args) {
+        
+        
+        int[] myarr= {12,45,67,89,23};
+        
+        for(int n:myarr)
+            System.out.println(n);
+        
+        SalesClass class1=new SalesClass();
+        class1.display();
+
+    }
+
+}
---
+++ firstApp/src/main/java/com/capg/devops/SalesClass.java
@@ -0,0 +1,17 @@
+package com.capg.devops;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SalesClass{
+	
+	List<Integer> list=Arrays.asList(54,56,213,35,65,7,6,2,4);
+	
+	public void display() {
+		System.out.println("****** List****");
+		for(int num:list) {
+			System.out.println(num);
+		}
+	}
+}
+
